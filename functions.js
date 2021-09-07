dcbe910f15ca07a0dcbdf8cb318352c09487d471
Ariel <arimezh@gmail.com>
2021-09-08 00:40:26 +0300

    Add range function

--- functions.js
+++ functions.js
@@ -1,1 +1,13 @@
 
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
+
+function range(grades) {
+    let lowest = grades[0], highest = grades[0]
+    for (const grade of grades) {
+        if (grade<lowest)
+            lowest = grade
+        if (grade>highest)
+            highest = grade
+    }
+    return (highest - lowest)
+}
